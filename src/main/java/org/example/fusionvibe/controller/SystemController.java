d35550200a8b94e8834b147ed4de9092476b85df
wdwdwq <qkenr7278@gmail.com>
2024-07-18 11:00:41 +0900

    화면 띄우기

--- src/main/java/org/example/fusionvibe/controller/SystemController.java
+++ src/main/java/org/example/fusionvibe/controller/SystemController.java
@@ -1,4 +1,12 @@
 package org.example.fusionvibe.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class SystemController {
+    @RequestMapping("/")
+    public String mainPage() {
+        return "main";
+    }
 }
